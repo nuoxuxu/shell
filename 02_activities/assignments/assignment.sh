8ad49922487bfcdc92ba0773903427414f311a45
Nuo Xu <xv.nuo@icloud.com>
2026-03-08 17:37:10 -0400

    Refactor assignment script to streamline data processing steps and add inventory file creation

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -28,22 +28,26 @@ unzip -q rawdata.zip
 # Complete assignment here
 
 # 1. Create a directory named data
-
+mkdir data
 # 2. Move the ./rawdata directory to ./data/raw (eg. move it into ./data and rename it to raw)
-
+mv ./rawdata data/raw
 # 3. List the contents of the ./data/raw directory
-
+ls data/raw
 # 4. Create the directory ./data/processed, 
 #    then create the following sub-directories within it: server_logs, user_logs, and event_logs
-
+mkdir -p data/processed/server_logs
+mkdir -p data/processed/user_logs
+mkdir -p data/processed/event_logs
 # 5. Copy all server log files (files with "server" in the name AND a .log extension) from ./data/raw to ./data/processed/server_logs
-
+ls data/raw/*server*.log | xargs -I {} cp {} data/processed/server_logs
 # 6. Repeat the above step for user logs and event logs
-
+ls data/raw/*user*.log | xargs -I {} cp {} data/processed/user_logs
+ls data/raw/*event*.log | xargs -I {} cp {} data/processed/event
 # 7. For user privacy, remove all files containing IP addresses (files with "ipaddr" in the filename) from ./data/raw and ./data/processed/user_logs
-
+find data/raw -type f -name "*ipaddr*" -delete
+find data/processed/user_logs -type f -name "*ipaddr*" -delete
 # 8. Create a file named ./data/inventory.txt that lists all the files in the subfolders of ./data/processed
-
+ls data/processed/*/* > data/inventory.txt
 
 ###########################################
 
